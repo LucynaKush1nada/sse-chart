5781f252da904ea45b5f9621dbea4b1996462168
Stas Oskol <stanislav.osk@mail.ru>
2023-09-18 20:59:21 +0300

    определение компонента графика в отдельный файл

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import * as React from 'react';
 import './App.css';
 
+import Chart from './Chart';
+
 import { fetchEventSource } from "@microsoft/fetch-event-source";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
 import IData from './IData';
 
 const serverBaseURL = "http://localhost:5000";
@@ -54,15 +47,7 @@ const App = () => {
   return (
     <div style={{ display: "grid", placeItems: "center" }}>
       <h1>Stock prices of a and b</h1>
-      <LineChart width={1000} height={400} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="time" />
-        <YAxis domain={[20, 26]} />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="aTechStockPrice" stroke="#8884d8" />
-        <Line type="monotone" dataKey="bTechStockPrice" stroke="#82ca9d" />
-      </LineChart>
+      <Chart data={data}/>
       <button onClick={() => changeFilter("a")}>Display A Price</button>
       <button onClick={() => changeFilter("b")}>Display B Price</button>
       <button onClick={() => changeFilter("both")}>Display both Price's</button>
---
+++ src/Chart.tsx
@@ -0,0 +1,29 @@
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+} from "recharts";
+
+export interface ChartProps {
+    data: any;
+}
+
+const Chart = ({ data }: ChartProps) => {
+    return (
+        <LineChart width={1000} height={400} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="time" />
+            <YAxis domain={[20, 26]} />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="aTechStockPrice" stroke="#8884d8" />
+            <Line type="monotone" dataKey="bTechStockPrice" stroke="#82ca9d" />
+        </LineChart>
+    )
+}
+
+export default Chart;
